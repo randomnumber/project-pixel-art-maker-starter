aef139e46205f4602cf195209ac1ef35f0ac6ddb
randomnumber <peter@helion.net>
2020-04-13 21:45:30 +1000

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,8 +1,8 @@
 // declare variables for each of the
 // html elements we will be using
 var sizer = document.getElementById('sizePicker');
-var rows = document.getElementById('inputHeight');
-var cols = document.getElementById('inputWidth');
+var height = document.getElementById('inputHeight');
+var width = document.getElementById('inputWidth');
 var colour = document.getElementById('colorPicker'); //English spelling! ;-)
 var grid = document.getElementById('pixelCanvas');
 
@@ -10,21 +10,22 @@ var grid = document.getElementById('pixelCanvas');
 // Select size input
 makeGrid(20,20); //default grid
 
-
 // When size is submitted by the user, call makeGrid()
 sizer.addListenerEvent('submit', function(event) {
   event.preventDefault();
+  var rows = height.value;
+  var cols = width.value;
   makeGrid(rows,cols);
 })
 
-
 function makeGrid(rows, cols) {
   // outer loop on the rows (y-dimension)
-  for (var y = 0; y < rows.value; y++) {
+  for (var y = 0; y < rows; y++) {
     var row = grid.insertRow(y)
     // inner loop to add the cells in each row (x-dimension)
-    for (var x = 0; x < cols.value; x++) {
+    for (var x = 0; x < cols; x++) {
       row.insertCell(x);
     }
   }
 }
+ß
