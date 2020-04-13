8e1797e9b54627f1661ccd3975a0af31c7d281e6
randomnumber <peter@helion.net>
2020-04-13 22:16:00 +1000

    Update designs.js

--- designs.js
+++ designs.js
@@ -7,6 +7,8 @@ var colour = document.getElementById('colorPicker'); //English spelling! ;-)
 var grid = document.getElementById('pixelCanvas');
 
 // Select color input
+userColour = colour.value;
+
 // Select size input
 makeGrid(5,5); //default grid
 
@@ -16,17 +18,24 @@ sizer.addEventListener('submit', function(event) {
   var rows = height.value;
   var cols = width.value;
   makeGrid(rows,cols);
-})
+});
 
 function makeGrid(rows, cols) {
-  // clear the default grid
+  // Clear the default grid
   grid.innerHTML = ""
-  // outer loop on the rows (y-dimension)
+  // Outer loop on the rows (y-dimension)
   for (var y = 0; y < rows; y++) {
     var row = grid.insertRow(y)
-    // inner loop to add the cells in each row (x-dimension)
+    // Inner loop to add the cells in each row (x-dimension)
     for (var x = 0; x < cols; x++) {
       row.insertCell(x);
     }
   }
 }
+
+// Listen for the user to click on a cell
+grid.addEventListener('click', function(event) {
+  if (event.target.nodeName === 'TD') {
+    event.target.style.background.Color = userColour
+  }
+});
